2880914c9dbe20537a316507dc2128d503a86f53
gsepe-dev <162506213+gsepe-dev@users.noreply.github.com>
2025-04-01 18:52:38 +0200

    fix: clean-up in configuration file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
